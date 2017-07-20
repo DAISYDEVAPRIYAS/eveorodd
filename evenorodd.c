51c9fe249363196877c35f329f0db758ecf6145d
DAISYDEVAPRIYAS <christypriya2607@gmail.com>
2017-07-20 13:28:55 +0530

    Create evenorodd.c

---
+++ evenorodd.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+	int a;
+	scanf("%d",&a);
+	if(a%2===0)
+	{
+		printf("%d is even",a);
+	}
+	else
+	{
+		printf("%d is odd",a);
+	}
+	// your code goes here
+	return 0;
+}
